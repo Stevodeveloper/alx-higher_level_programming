bebe54b5bf7b22225c1b8ef366c9daa1f45da55d
Stevodeveloper <stepheneoduor20@gmail.com>
2022-08-25 13:16:45 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	counter = len(sys.argv)
+	from sys import argv
+	counter = len(argv)
 	
 	if counter == 2:
 		print('{} argument:'.format(counter - 1))
@@ -11,4 +11,4 @@ if __name__ == "__main__":
 		print('{} arguments:'.format(counter - 1))
 
 	for i in range(1, counter):
-		print('{}: {}'.format(i, sys.argv[i]))
+		print('{}: {}'.format(i, argv[i]))
