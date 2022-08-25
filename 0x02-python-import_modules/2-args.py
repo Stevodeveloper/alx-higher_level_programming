98c064cf705012432f38aa35386bbdc5f8c84edf
Stevodeveloper <stepheneoduor20@gmail.com>
2022-08-25 12:23:05 +0000

    command line args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	import sys
+	counter = len(sys.argv)
+	
+	if counter == 1:
+		print('{} argument:'.format(counter - 1))
+	elif counter == 0:
+		print('{} arguments.'.format(counter - 1))
+	else:
+		print('{} arguments:'.format(counter - 1))
+
+	for i in range(1, counter):
+		print('{}: {}'.format(i, sys.argv[i]))
