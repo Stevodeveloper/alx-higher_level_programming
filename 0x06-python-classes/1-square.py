a87d37e87dd65d4b7cf5ef234ea19adf74ad20c5
Stevodeveloper <stepheneoduor20@gmail.com>
2022-09-15 18:02:19 +0000

    attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """square"""
+    def __init__(self, size):
+        """
+        initialize square attributes
+        Args:
+        size (int): size of square
+        Returns:
+        None
+        """
+
+        self.__size = size
+
