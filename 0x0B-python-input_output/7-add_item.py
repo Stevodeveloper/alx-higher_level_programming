bfb0c0b6d1169ffa4ebc9633bb936684207d8c30
Stevodeveloper <stepheneoduor20@gmail.com>
2022-09-28 11:43:13 +0000

    add item to json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+from os import path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+a = []
+if path.exists("add_item.json"):
+    a = load_from_json_file("add_item.json")
+a = a + argv[1:]
+save_to_json_file(a, "add_item.json")
+
