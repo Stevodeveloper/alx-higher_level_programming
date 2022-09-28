8d90e8157b0a1aad2cd0ae6a8f75b0f49303eb28
Stevodeveloper <stepheneoduor20@gmail.com>
2022-09-28 11:20:29 +0000

    save sa json object to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,6 @@ def save_to_json_file(my_obj, filename):
 		filename - name of the destination file where the object will be saved
 	"""
 	with open(filename, mode = "w") as f:
-		return json.dumps(my_obj, f)
+		a = json.dumps(my_obj)
+		f.write(a)
 
